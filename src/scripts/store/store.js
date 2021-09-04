e0b1dbfec9fadb97a0c7ccb197c328f6626f30dc
Rivaldi <rvdans002@gmail.com>
2021-09-04 19:11:07 +0700

    Update store.js functionality

--- src/scripts/store/store.js
+++ src/scripts/store/store.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex';
+import Moment from 'moment';
 import CONFIG from '../global/config';
 import TodoData from '../data/TodoData';
 
@@ -15,10 +16,6 @@ const todo = {
     setTodo(state, todo) {
       state.all = todo;
     },
-
-    setTodoFinished(state, todo) {
-
-    },
   },
 
   actions: {
@@ -27,17 +24,39 @@ const todo = {
       const todoList = TodoData.getTodo(CONFIG.TODO_STORAGE_KEY);
       
       ctx.commit('setTodo', todoList);
-    }
+    },
+
+    setTodoFinished(ctx, finishedTodo) {
+      const todoList = TodoData.getTodo(CONFIG.TODO_STORAGE_KEY);
+      const finishedTodoIndex = todoList.findIndex((todo) => todo.id === finishedTodo.id);
+
+      const now = Moment();
+
+      todoList[finishedTodoIndex].finished = true;
+      todoList[finishedTodoIndex].finishedAt = `${now.date()}-${now.month()}-${now.year()} ${now.hour()}:${now.minute()}`;
+
+      TodoData.storeAllTodo(CONFIG.TODO_STORAGE_KEY, todoList)
+
+      ctx.commit('setTodo', todoList);
+    },
   },
 
   getters: {
-    finishedList(state) {
-      
+    getAll(state) {
+      return state.all;
+    },
+
+    getUnfinishedList(state) {
+      return state.all.filter((todo) => !todo.finished);
+    },
+    
+    getFinishedList(state) {
+      return state.all.filter((todo) => todo.finished);
     },
 
     todoListLength(state) {
       return state.all.length;
-    }
+    },
   }
 };
 
